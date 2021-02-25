f090dd6e3e4f1b82b76db1ab9d2046c79d5c3384
jfremond <jfremond@student.42.fr>
2021-02-25 17:54:06 +0100

    Adding sources/ft_treat_int.c

---
+++ sources/ft_treat_int.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_treat_int.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 17:49:14 by jfremond          #+#    #+#             */
+/*   Updated: 2021/02/25 17:53:56 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+void	ft_treat_int(t_struct *s, va_list args)
+{
+	long	num;
+
+	num = (long)va_arg(args, int);
+	if (num < 0)
+	{
+		num *= -1;
+		s->neg = 1;
+		s->width--;
+		s->len++;
+	}
+	s->res = ft_itoa_base(num, "0123456789");
+	while (s->res[s->nbc])
+	{
+		s->nbc++;
+		s->len++;
+	}
+	ft_printf_width_num(s->res, s);
+	free(s->res);
+	s->i++;
+}
